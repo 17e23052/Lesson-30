a110e5582cc6c0fe75a9542a7741d4dcc4efd9c6
Mathman <>
2021-05-19 08:39:28 +0000

    Deck of cards

---
+++ main.py
@@ -0,0 +1,16 @@
+suits = ["♥", "♦", "♣", "♠"]
+deck = []
+for suit in suits:
+  for x in range(1,14):
+    if x == 1:
+      value = "A"
+    elif x == 11:
+      value = "J"
+    elif x == 12:
+      value = "Q"
+    elif x == 13:
+      value = "K"
+    else:
+      value = str(x)
+    deck.append(value + suit)
+print(deck)
